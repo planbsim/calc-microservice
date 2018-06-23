f0a6f184a54db34c8083c921e372c4c059d49757
Simon Planberger <simon.planberger@ecx.io>
2018-06-23 19:37:37 +0200

    Fix entrypoint.sh

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /WebCalculator.Api/WebCalculator.Api/publish
-dotnet WebCalculator.Api.dll --server.urls=http://0.0.0.0:${PORT-"8000"}
+cd WebCalculator.Api/WebCalculator.Api/publish
+dotnet WebCalculator.Api.dll
